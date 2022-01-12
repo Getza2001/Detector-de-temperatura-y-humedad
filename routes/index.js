958453ccac42f47e559571ff50944053c8075be9
Luis-Donato-Martinez-Simuta <74307313+Luis-Donato-Martinez-Simuta@users.noreply.github.com>
2022-01-12 02:54:02 -0600

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,111 @@
 var express = require('express');
 var router = express.Router();
-//var db = require('../config/database');
+var UsuarioDAO = require('../model/UsuarioDAO') //El DAO del usuario para operaciones con la base de datos
+var md5 = require("md5");  //Libreria para ioncriptar contraseñas
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index');
 });
 
+router.get('/login', function (req, res, next) {
+  console.log("Cerrando sesions");
+  res.render('index');
+});
 
-router.get('/main', function(req, res, next) {
-  //Token de seguridad
-  let tokenSeguridad = req.body.tokenSeguridad;
-  if(tokenSeguridad == undefined){
-    res.render('index');
-  }
+router.get('/main/:IdUsuario', function (req, res, next) {
+  
+  let IdUsuario = req.params.IdUsuario;
+  console.log("Id parametro index main: ", IdUsuario);
+  UsuarioDAO.obtenerUsuarioPorId(IdUsuario , (data)=>{
+    let usuario = data;
+
+    res.render('lectura',{
+      usuario : usuario
+    })
+
+  });
+
 
 });
 
-router.post('/main', function(req, res, next) {
-  //Login del usuario
+router.get('/about/:IdUsuario', function (req, res, next) {
+  let IdUsuario = req.params.IdUsuario;
+  console.log("Id del parametro en index", IdUsuario);
+  UsuarioDAO.obtenerUsuarioPorId(IdUsuario ,(data)=>{
+    //Capturamos al usuario es esta vriable
+    let usuario = data;
+    //console.log("Usuario: ",usuario);
+
+    res.render('about',{
+      usuario : usuario
+    });
+
+  });
+  
+});
+
+
+
+router.post('/main', function (req, res, next) {
+  
+  //Guardamos el nombre de usuario y contraseña puestos en el login 
   let usuario = {
-    username : req.body.username,
-    password : req.body.password,
+    username: req.body.username,       //Nombre del usuario dado por el usuario
+    password: md5(req.body.password),  //Constraseña dada por el usuario, la mandamos a incriptar con md5
   };
-
-  if(usuario.username == 'root' && usuario.password == 'root'){
-    /*
-    db.ref('datos').on('value',(snapshot) =>{
-      let datos = snapshot.val();
-      console.log(datos)
-      
-    });
-    */
-    res.render('lectura');
     
-  }else{
-    res.render('index',{
-      acceso:false
-    });
-  }
+  UsuarioDAO.logueo(usuario, (data) => {
 
-  //res.render('lectura');
+    //Capturamos al usuario es esta vriable
+    usuario = data;
+    //console.log("Usuario: ",usuario);
+
+    //Validamos que no sea un valor indefinido
+    if (usuario != undefined) {
+      //Validando si es usuario de tipo administrador 
+      if (usuario.tipoUsuario === 1) { 
+        //Si el usuario es de tipo administrador entra al sistema
+        res.render('lectura', {
+          usuario: usuario 
+        });
+      } else {
+        //De no ser administrador lo mandamos de regreso al login con 
+        //Con un mensaje de error de auntentificacion
+        res.render('index', {
+          acceso: false
+        });
+      }
+    } else {
+      res.render('index', {
+        acceso: false
+      });
+    }
+
+
+  });
+
 });
+
+
+router.get('/construccion', function (req, res, next) {
+  res.render('construccion');
+});
+
+
+
+router.get('/reporte/:IdUsuario', function (req, res, next) {
+
+  let IdUsuario = req.params.IdUsuario;
+
+  UsuarioDAO.obtenerUsuarioPorId(IdUsuario , (data)=>{
+    let usuario = data;
+
+    res.render('reporteLectura',{
+      usuario: usuario
+    });
+  });
+
+});
+
 
 module.exports = router;
